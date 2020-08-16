cdc3567f56075786990da32b85f87a53f18e6e06
L Yuda Rahmani Karnaen <33549743+yudartcode@users.noreply.github.com>
2020-08-16 20:01:57 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Route
+| API Route ¹
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
