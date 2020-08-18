7a829a2b6d5d8b9061496fac80b97b3a6069533b
L Yuda Rahmani Karnaen <33549743+yudartcode@users.noreply.github.com>
2020-08-18 19:59:15 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 | API Route ¹
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register API routes for your application.
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
