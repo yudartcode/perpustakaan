80952f92df53f1764e52ec9d58bdc7f0275a9451
L Yuda Rahmani Karnaen <33549743+yudartcode@users.noreply.github.com>
2020-09-08 22:02:17 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application.
 | routes are loaded by the RouteServiceProvider within a group
 | is assigned the "api" middleware group. Enjoy building your
-|
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
